47c3e62a924760b94ebdee1bad9127e5480962ac
Romário Melo <romario.melo.barreiras@gmail.com>
2026-03-23 21:20:42 -0300

    refactor(app): change dockerfile and add cors origin

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -7,12 +7,19 @@ import { errorHandler } from '@infrastructure/http/middlewares/error-handler.mid
 function createServer() {
   const app = fastify({ logger: true });
 
-  app.register(cors);
+  app.register(cors, {
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://172.18.0.2:5173'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Authorization', 'Content-Type']
+  });
+
   app.register(authRoutes, { prefix: '/auth' });
   app.register(postRoutes, { prefix: '/posts' });
-
   app.get('/health', async () => ({ status: 'ok' }));
-
   app.setErrorHandler(errorHandler);
 
   return app;
